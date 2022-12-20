33fd06368aafb1d22014a86d8731678494ee148e
ManishMaurya22 <49672294+ManishMaurya22@users.noreply.github.com>
2022-12-20 12:07:46 +0530

    fixing stuff

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow incoming http and https traffic and Allow everything out"
 
-  vpc_id = data.aws_vpc.default.true 
+  vpc_id = data.aws_vpc.default.blog 
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
